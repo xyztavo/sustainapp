a12ee3e79e12d4ecd0f8992c224516d394310fe2
xyztavo <gustavobreasy@gmail.com>
2025-11-28 21:26:02 -0300

    feat: Add button to add items and RecyclerView to HomeFragment layout

--- app/src/main/java/br/com/etecia/sustainapp/ui/HomeFragment.java
+++ app/src/main/java/br/com/etecia/sustainapp/ui/HomeFragment.java
@@ -9,10 +9,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
 
 import br.com.etecia.sustainapp.R;
-import br.com.etecia.sustainapp.databinding.FragmentHomeBinding;
+import br.com.etecia.sustainapp.data.SmartItem;
+import br.com.etecia.sustainapp.data.SmartItemAdapter;
+import br.com.etecia.sustainapp.data.SmartViewModel;
 
 /**
  * Fragment Home que exibe cards explicando o conceito de Cidades Inteligentes.
@@ -20,6 +26,8 @@ import br.com.etecia.sustainapp.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private SmartViewModel viewModel;
+    private SmartItemAdapter adapter;
 
     @Nullable
     @Override
@@ -34,14 +42,49 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.d("HomeFragment", "onViewCreated");
 
+        viewModel = new ViewModelProvider(requireActivity()).get(SmartViewModel.class);
+        adapter = new SmartItemAdapter(new ArrayList<>(), new SmartItemAdapter.OnItemClickListener() {
+            @Override
+            public void onEdit(int position) {
+                Log.d("HomeFragment", "Edit clicked for position: " + position);
+                SmartItem item = viewModel.getSmartItems().getValue().get(position);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", item.getName());
+                bundle.putString("category", item.getCategory());
+                bundle.putString("description", item.getDescription());
+                bundle.putInt("position", position);
+                Navigation.findNavController(view).navigate(R.id.action_home_to_cadastro, bundle);
+            }
+
+            @Override
+            public void onDelete(int position) {
+                Log.d("HomeFragment", "Delete clicked for position: " + position);
+                viewModel.removeItem(position);
+            }
+        });
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerView.setAdapter(adapter);
+        Log.d("HomeFragment", "RecyclerView set up");
+
+        viewModel.getSmartItems().observe(getViewLifecycleOwner(), smartItems -> {
+            Log.d("HomeFragment", "Items updated, size: " + smartItems.size());
+            adapter.updateItems(smartItems);
+        });
+
         binding.card1.setOnClickListener(v -> {
             Log.d("HomeFragment", "Card1 clicked, navigating to cadastro");
             Navigation.findNavController(view).navigate(R.id.action_home_to_cadastro);
         });
 
         binding.card2.setOnClickListener(v -> {
-            Log.d("HomeFragment", "Card2 clicked, navigating to lista");
-            Navigation.findNavController(view).navigate(R.id.action_home_to_lista);
+            Log.d("HomeFragment", "Card2 clicked");
+            // Optional
+        });
+
+        binding.btnAddItem.setOnClickListener(v -> {
+            Log.d("HomeFragment", "Add item clicked");
+            Navigation.findNavController(view).navigate(R.id.action_home_to_cadastro);
         });
     }
 
